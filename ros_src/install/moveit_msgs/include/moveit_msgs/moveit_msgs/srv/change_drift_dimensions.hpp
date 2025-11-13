91a1b8ee06f330f57ddbefdd738716465af0b096
cananella <mango7460@naver.com>
2025-11-13 14:02:00 +0900

    submodule install

---
+++ ros_src/install/moveit_msgs/include/moveit_msgs/moveit_msgs/srv/change_drift_dimensions.hpp
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_cpp/resource/idl.hpp.em
+// generated code does not contain a copyright notice
+
+#ifndef MOVEIT_MSGS__SRV__CHANGE_DRIFT_DIMENSIONS_HPP_
+#define MOVEIT_MSGS__SRV__CHANGE_DRIFT_DIMENSIONS_HPP_
+
+#include "moveit_msgs/srv/detail/change_drift_dimensions__struct.hpp"
+#include "moveit_msgs/srv/detail/change_drift_dimensions__builder.hpp"
+#include "moveit_msgs/srv/detail/change_drift_dimensions__traits.hpp"
+#include "moveit_msgs/srv/detail/change_drift_dimensions__type_support.hpp"
+
+#endif  // MOVEIT_MSGS__SRV__CHANGE_DRIFT_DIMENSIONS_HPP_
